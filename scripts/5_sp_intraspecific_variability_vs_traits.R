975a89ea0d355ece8fd2234e0f144206b690a78f
Marie Gutgesell <marie.gutgesell11@gmail.com>
2026-04-02 15:55:52 +0200

    species intraspecific variability contribution without separation by habitat type

--- scripts/5_sp_intraspecific_variability_vs_traits.R
+++ scripts/5_sp_intraspecific_variability_vs_traits.R
@@ -9,7 +9,7 @@ load("data/SpeciesIntraspecific_variance_perSite_Env.RData")
 
 ##Calculate the mean instraspecific variability of each species across all food webs 
 sp_var_mean <- SpVar_env %>%
-  group_by(fish_species, waterbody_type) %>% ##separated by ecosystem type- but this may not be necessary 
+  group_by(fish_species) %>% ##separated by ecosystem type- but this may not be necessary 
   filter(!is.na(sp_site_var_N)) %>% 
   summarise_at(vars(sp_site_var_C, sp_site_var_N), list(mean = mean, sd = sd))
 
