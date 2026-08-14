8a8898b1627d80b72beaa57360984da3f420764c
Marie Gutgesell <marie.gutgesell11@gmail.com>
2026-08-14 09:27:08 +0200

    move sorting from julien to beginning to resolve incorrect sp_site column

--- scripts/0_fish_df_formation.R
+++ scripts/0_fish_df_formation.R
@@ -28,21 +28,81 @@ test <- DataFish %>%
   filter(is.na(fish_scientific_name))
 ##NAs come from cases where fish were not idenfied (e.g., mix of rudd and mosquitofish)
 
+##1) read in Julians sorting and clean up accordingly
+julien_site_sorting <- read_excel("data/Sites_Julien_27.07.2026.xlsx") %>%
+  select(FWB_id, Use, Reason, Action, Action_description) %>%
+  distinct(FWB_id, .keep_all = TRUE)
+head(julien_site_sorting)
+
+##number of sites with mixed taxonomy levels that were included
+julien_site_sorting %>%
+  filter(Use == "YES") %>%
+  select(FWB_id) %>%
+  distinct() %>%
+  count()
+
+actions <- julien_site_sorting %>% 
+  select(Action_description) %>%
+  distinct()
+
+#sites_remove <- julien_site_sorting %>%
+#  filter(Use == "NO")
+
+DataFish_2 <- DataFish %>%
+  left_join(julien_site_sorting, by = "FWB_id") %>%
+  mutate(Use = coalesce(Use, "YES")) %>% ##if Use is NA (i.e., sites not in sorting table as all species-level) then keep site
+  filter(Use != "NO") %>% ##remove sites marked for removal
+  filter (case_when(
+    Action_description == "Remove individuals with NA" ~ !is.na(fish_species) & !is.na(fish_genus) &!is.na(fish_family),
+    Action_description %in% c(
+      "Luciobarbus steindachneri; Remove individual with NA",
+      "Luciobarbus microcephalus; Remove individual with NA",
+      "Luciobarbus sclateri; Remove individual with NA"
+    ) ~
+      !is.na(fish_species) |
+      fish_genus == "Luciobarbus",
+    
+    # All other rows are retained
+    TRUE ~ TRUE
+  )) %>%
+  
+  # !(coalesce(Action_description == "Remove individuals with NA", FALSE) & (is.na(fish_species) | is.na(fish_genus) |is.na(fish_family)))) %>% #remove an individual if any taxonomic field is NA 
+  mutate(
+    fish_scientific_name = case_when(
+      
+      # Keep genus-level identification and add "sp."
+      Action_description == "Keep genus and add sp." & fish_name_level == "genus" ~ str_squish(paste(fish_genus, "sp.")),
+      
+      # Keep family-level identification and add "sp."
+      Action_description == "Keep family and add sp." & fish_name_level == "family" ~ str_squish(paste(fish_family, "sp.")),
+      
+      ##Add species level identification for specific cases 
+      Action_description == "Luciobarbus steindachneri" & fish_genus == "Luciobarbus" ~ "Luciobarbus steindachneri",
+      
+      Action_description == "Luciobarbus steindachneri; Remove individual with NA" & fish_genus == "Luciobarbus" ~ "Luciobarbus steindachneri",
+      Action_description == "Luciobarbus microcephalus; Remove individual with NA"& fish_genus == "Luciobarbus" ~ "Luciobarbus microcephalus",
+      Action_description == "Luciobarbus sclateri; Remove individual with NA"& fish_genus == "Luciobarbus" ~ "Luciobarbus sclateri",
+      # Otherwise retain the existing name
+      TRUE ~ fish_scientific_name ##for all species level(i.e., fish_name_level == species) it should already be there and all good, nothing to do 
+    ))
+
+
+
 ##add species-site identifier column
-DataFish$sp_site<-paste(DataFish$fish_scientific_name,DataFish$collection_site_id,sep="_")
+DataFish_2$sp_site<-paste(DataFish_2$fish_scientific_name,DataFish_2$collection_site_id,sep="_")
 ##Assign 1 - number of fish
-DataFish$num<-1
+DataFish_2$num<-1
 
 
 
 ##Check for and remove temporal replication within site -- want to ensure only 1 year of data
-DataFish <- DataFish %>%
+DataFish_2 <- DataFish_2 %>%
   # mutate(as.Date(collection_date)) %>%
   mutate(is_range = str_detect(collection_date, "^\\d{4}-\\d{4}$")) %>% ##indicate if sample collection date is a range of years
   mutate(year = str_extract(collection_date, "^\\d{4}") %>% as.numeric()) ##create column of year
 
 ##calculate the number of sampling years
-site_num_sampling_years <- DataFish %>%
+site_num_sampling_years <- DataFish_2 %>%
   filter(is_range == FALSE) %>% ##remove any sites that only had collection date listed as year ranges 
   select(FWB_id, collection_site_id, year) %>%
   unique() %>%
@@ -55,11 +115,11 @@ num_sites_more1yr <- site_num_sampling_years %>%
   filter(number_sampling_years > 1)
 
 ##join number of sampling years to main fish df 
-DataFish <- left_join(DataFish, site_num_sampling_years, by = c("FWB_id", "collection_site_id"))
+DataFish_2 <- left_join(DataFish_2, site_num_sampling_years, by = c("FWB_id", "collection_site_id"))
 
 
 ##calculate the number of individuals sampled per species within each site-year 
-datafish_summary_sp <- DataFish %>%
+datafish_summary_sp <- DataFish_2 %>%
   group_by(FWB_id, collection_site_id, year, number_sampling_years, fish_scientific_name) %>%
   count() %>%
   rename(num_ind_per_species = "n")
@@ -93,7 +153,7 @@ annual_site_list <- datafish_summary_site %>%
 ##936 when NA sp name are removed (fewer sites because some sites only have genus level ID)
 
 ##Select the site and year from datafish
-datafish_annual <- DataFish %>%
+datafish_annual <- DataFish_2 %>%
   mutate(site_year_code = paste(FWB_id, year, sep = "_")) %>%
   filter(site_year_code %in% annual_site_list$site_year_code) %>%
   left_join(datafish_summary_site, by = c("FWB_id", "collection_site_id", "year", "number_sampling_years")) 
@@ -243,71 +303,17 @@ cutoff_site_list <- summary_all_cutoffs %>%
   mutate(site_year_code = paste(FWB_id, year, sep = "_")) %>%
   filter(min_sp_num == 2) %>% ##select minimum species number
   filter(cutoff == "75pct") ##select cutoff 
-##453 sites 
+##436 sites 
 
 ##select site/years from chosen cutoff 
-DataFish_2 <- datafish_annual %>%
+DataFish_final <- datafish_annual %>%
   filter(site_year_code %in% cutoff_site_list$site_year_code)
 
+
+
+
+
 ##Correcting to final sites:
-##1) read in Julians sorting and clean up accordingly
-julien_site_sorting <- read_excel("data/Sites_Julien_27.07.2026.xlsx") %>%
-  select(FWB_id, Use, Reason, Action, Action_description) %>%
-  distinct(FWB_id, .keep_all = TRUE)
-head(julien_site_sorting)
-
-##number of sites with mixed taxonomy levels that were included
-julien_site_sorting %>%
-  filter(Use == "YES") %>%
-  select(FWB_id) %>%
-  distinct() %>%
-  count()
-
-actions <- julien_site_sorting %>% 
-  select(Action_description) %>%
-  distinct()
-
-#sites_remove <- julien_site_sorting %>%
-#  filter(Use == "NO")
-
-DataFish_final <- DataFish_2 %>%
-  left_join(julien_site_sorting, by = "FWB_id") %>%
-  mutate(Use = coalesce(Use, "YES")) %>% ##if Use is NA (i.e., sites not in sorting table as all species-level) then keep site
-  filter(Use != "NO") %>% ##remove sites marked for removal
-  filter (case_when(
-    Action_description == "Remove individuals with NA" ~ !is.na(fish_species) & !is.na(fish_genus) &!is.na(fish_family),
-    Action_description %in% c(
-      "Luciobarbus steindachneri; Remove individual with NA",
-      "Luciobarbus microcephalus; Remove individual with NA",
-      "Luciobarbus sclateri; Remove individual with NA"
-    ) ~
-      !is.na(fish_species) |
-      fish_genus == "Luciobarbus",
-    
-    # All other rows are retained
-    TRUE ~ TRUE
-    )) %>%
-    
-   # !(coalesce(Action_description == "Remove individuals with NA", FALSE) & (is.na(fish_species) | is.na(fish_genus) |is.na(fish_family)))) %>% #remove an individual if any taxonomic field is NA 
-  mutate(
-    fish_scientific_name = case_when(
-      
-      # Keep genus-level identification and add "sp."
-      Action_description == "Keep genus and add sp." & fish_name_level == "genus" ~ str_squish(paste(fish_genus, "sp.")),
-      
-      # Keep family-level identification and add "sp."
-      Action_description == "Keep family and add sp." & fish_name_level == "family" ~ str_squish(paste(fish_family, "sp.")),
-      
-      ##Add species level identification for specific cases 
-      Action_description == "Luciobarbus steindachneri" & fish_genus == "Luciobarbus" ~ "Luciobarbus steindachneri",
-    
-      Action_description == "Luciobarbus steindachneri; Remove individual with NA" & fish_genus == "Luciobarbus" ~ "Luciobarbus steindachneri",
-      Action_description == "Luciobarbus microcephalus; Remove individual with NA"& fish_genus == "Luciobarbus" ~ "Luciobarbus microcephalus",
-      Action_description == "Luciobarbus sclateri; Remove individual with NA"& fish_genus == "Luciobarbus" ~ "Luciobarbus sclateri",
-      # Otherwise retain the existing name
-      TRUE ~ fish_scientific_name ##for all species level(i.e., fish_name_level == species) it should already be there and all good, nothing to do 
-    ))
-  
 
 
 
@@ -358,6 +364,30 @@ rare_sp %>%
 
 
 
+##checking into Luciobarbus steindachneri
+ls1 <- DataFish %>%
+  filter(fish_scientific_name == "Luciobarbus sclateri") %>% 
+  select(FWB_id, fish_scientific_name) %>%
+  distinct()
 
 
+ls2 <- DataFish_2 %>%
+  filter(fish_scientific_name == "Luciobarbus sclateri") %>% 
+  select(FWB_id, fish_scientific_name) %>%
+  distinct()
 
+ls3 <- DataFish_final %>%
+  filter(fish_scientific_name == "Luciobarbus sclateri") %>% 
+  select(FWB_id, fish_scientific_name) %>%
+  distinct()
+
+#Luciobarbus sclateri
+
+##test - went from 437 to 436 which site is missing?
+#sites_437 <- read.csv("data/SiteVar_2sp_75cutoff.csv")  %>%
+#  select(FWB_id) %>%
+#  distinct()
+#sites_436 <- cutoff_site_list
+
+#test <- anti_join(sites_437, sites_436, by = "FWB_id")
+##FWB_0757 -- why did this site disappear? because only 70% of its species (less than 75% have more than 3 individuals)
